25d580177401df660e8c18f4b2d9bda2c24c920f
Samuel Bosch <samuel.bosch@gmail.com>
2013-09-06 13:32:19 +0200

    implementing death, start implementing immigration but stuck in immutability land

--- TestRun/TestRun/MultiSwarmOptimization.fs
+++ TestRun/TestRun/MultiSwarmOptimization.fs
@@ -57,15 +57,21 @@ module MultiSwarmOptimization =
     let inline (.*) (a:float) b = List.map ((*) a) b
     let inline (++) (a:float list) b = List.map2 (+) a b
     let inline (--) (a:float list) b = List.map2 (-) a b
-    
-    let updateParticle bestMultiPos bestSwarmPos (particle:Particle) = 
+    let inline (..*) (a:float list) b = List.map2 ((*)) a b
+
+    let updateParticle multiSwarm bestMultiPos bestSwarmPos (particle:Particle) =   
         let w  = 0.729
         let c1, c2, c3 = 1.49445,1.49445, 0.3645
-        let r1, r2, r3 = rand.NextDouble(), rand.NextDouble(), rand.NextDouble()
+        let dim = particle.Position.Length
+        let createR() = List.init multiSwarm.Dim (fun i -> rand.NextDouble())
+        let r1, r2, r3 = createR(), createR(), createR()
         let newVelocity = (w .* particle.Velocity)
-                            ++ (c1 * r1 .* (particle.Best.Pos -- particle.Position))
-                            ++ (c2 * r2 .* (bestSwarmPos -- particle.Position))
-                            ++ (c3 * r3 .* (bestMultiPos -- particle.Position))
+                            ++ (c1 .* r1 ..* (particle.Best.Pos -- particle.Position))
+                            ++ (c2 .* r2 ..* (bestSwarmPos -- particle.Position))
+                            ++ (c3 .* r3 ..* (bestMultiPos -- particle.Position))
+        // keep velocity within bounds
+        let newVelocity = newVelocity |> List.map ((max multiSwarm.MinX) >> (min multiSwarm.MaxX))
+        // update position
         let newPosition = particle.Position ++ newVelocity
         let newCost = cost newPosition
         
@@ -74,12 +80,23 @@ module MultiSwarmOptimization =
             Some(newBest), { particle with Velocity = newVelocity; Position=newPosition; Best=newBest}
         else
             None, { particle with Velocity = newVelocity; Position=newPosition }
-
     
-    let updateSwarm bestMultiPos swarm =
-        let folder (particle:Particle) state = //state makes sure that we always now the up to date best particle cost of this swarm
+    let deathOrAlive multiSwarm particle = 
+        let death = 0.005 // prob of particle death
+        if rand.NextDouble() < death then
+            let newParticle = Particle.create multiSwarm.Dim multiSwarm.MinX multiSwarm.MaxX
+            newParticle
+        else
+            particle
+
+    let updateSwarm multiSwarm bestMultiPos swarm =
+        let folder (particle:Particle) state = //state makes sure that we always know what the best particle cost of this swarm is
             let best, particles = state
-            let newBest, newParticle = updateParticle bestMultiPos best.Pos particle
+
+            let death = 0.005 // prob of particle death
+            let particle = deathOrAlive multiSwarm particle    
+            
+            let newBest, newParticle = updateParticle multiSwarm bestMultiPos best.Pos particle
             if newBest.IsSome then
                 newBest.Value, (newParticle::particles)
             else
@@ -90,12 +107,13 @@ module MultiSwarmOptimization =
             Some(newBest), { swarm with Particles = particles; Best=newBest}
         else
             None, { swarm with Particles = particles }
-
+    
     let updateMultiSwarm multiSwarm =
-        // TODO implement death and immigration
-        let folder (swarm:Swarm) state = //state makes sure that we always now the up to date best swarm cost
+        // TODO implement immigration
+        let folder (swarm:Swarm) state = //state makes sure that we always know what the best swarm cost is
             let best, swarms = state
-            let newBest, newSwarm = updateSwarm best.Pos swarm
+
+            let newBest, newSwarm = updateSwarm multiSwarm best.Pos swarm
             if newBest.IsSome then
                 newBest.Value, (newSwarm::swarms)
             else
@@ -106,13 +124,28 @@ module MultiSwarmOptimization =
         else
             { multiSwarm with Swarms = swarms }
 
-    
-    let rec solve (multiSwarm:MultiSwarm) maxloop = 
+    let immigration (swarms:Swarm list) (i, j) = 
+        let particleA = swarms.[i].Particles.[j]
+        let otheri = rand.Next(0, swarms.Length)
+        let otherj = rand.Next(0, swarms.Head.Particles.Length)
+        let particleB = swarms.[otheri].Particles.[otherj]
+        
+        let substituteParticle newParticle index swarm = { swarm with Particles = ( List.mapi (fun i particle -> if i = index then newParticle else particle) swarm.Particles) }
+        let swapParticle currentIndex swarm  = 
+            if currentIndex = i then 
+                (substituteParticle particleB j swarm) 
+            else if currentIndex = otheri then
+                (substituteParticle particleA otherj swarm) 
+            else swarm
+        List.mapi swapParticle swarms
+
+
+    let rec solve (multiSwarm:MultiSwarm) maxloop ij = 
         if maxloop <= 0 then
             multiSwarm
         else
-            let newMultiSwarm = updateMultiSwarm multiSwarm
-            solve newMultiSwarm (maxloop-1)
+            let updatedMultiSwarm = updateMultiSwarm multiSwarm
+            solve updatedMultiSwarm (maxloop-1) ij
         
     let run() =
         printfn "Begin Multiple Particle Swarm optimization demo"
@@ -125,7 +158,10 @@ module MultiSwarmOptimization =
         printfn "Initial multiswarm:"
         printfn "%A" ms
         let maxLoop = 150
-        let ms = solve ms maxLoop
+        let i_range = [0 .. numSwarms-1]
+        let j_range = [0 .. numParticles-1]
+        let ij = i_range |> List.collect (fun i -> j_range |> List.map (fun j -> (i,j)))
+        let ms = solve ms maxLoop ij
         printfn "Final multiswarm:"
         printfn "%A" ms
         printfn "Best solution found with cost = %f at x0 = %f, x1 = %f" ms.Best.Cost ms.Best.Pos.[0] ms.Best.Pos.[1]
